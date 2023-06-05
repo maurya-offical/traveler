87f92084756e074b8bbc9ffb17c49c4c6804c57d
Ashish Kumar <87820654+ashishalf@users.noreply.github.com>
2023-06-05 19:08:06 +0530

    Update profile.php
    Responsive Change Password

--- profile.php
+++ profile.php
@@ -12,77 +12,51 @@ include('includes/config.php');
 
 <head>
    <style>
-/* Style for the popup window */
-        .popup {
-  display: none; /* Hide the popup by default */
-  position: fixed;
-  z-index: 9999;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  /* overflow: auto; */
-  background-color: rgba(0, 0, 0, 0.5);
-}
+      /* Style for the popup window */
+      .popup {
+         display: none;
+         position: fixed;
+         left: 0;
+         top: 0;
+         width: 100%;
+         height: 100%;
+         background-color: rgba(0, 0, 0, 0.5);
+         z-index: 9999;
+      }
 
-.popup-content {
-  background-color: #ffffff;
-  margin: 10px auto;
-  padding: 20px;
-  max-width: 400px;
-  /* border-radius: 30px; */
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
-}
+      .popup-content {
+         background-color: #fefefe;
+         margin: 10% auto;
+         padding: 20px;
+         border: 1px solid #888;
+         width: 400px;
+          border-radius: 7px;
+      }
 
-.close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-  cursor: pointer;
-}
+      .close {
+         float: right;
+         font-size: 20px;
+         font-weight: bold;
+         cursor: pointer;
+      }
 
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-}
-
-h2 {
-  margin-top: 0;
-}
-
-form {
-  margin-top: 20px;
-}
-
-label {
-  display: block;
-  font-weight: bold;
-}
-
-input[type="email"],
-input[type="password"],
-input[type="submit"] {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 50px;
-  background-color: #eee;
-}
-
-input[type="submit"] {
-  background-color: #008ecf;
-  color: white;
-  cursor: pointer;
-}
-
-input[type="submit"]:hover {
-  background-color: #1db8ff;
-}
-
-</style>
+      .close:hover {
+         color: red;
+      }
+      input{
+          width:100%;
+      }
+      @media only screen and (max-width: 600px) {
+      /* Styles for screens with a maximum width of 600px */
+      .popup-content {
+         max-width: 100%;
+      }
+      .popup-content input {
+         width: 100%;
+      }
+   }
+      
+   </style>
 </head>
 
 <body>
@@ -156,17 +130,17 @@ input[type="submit"]:hover {
                               <div id="popup" class="popup">
                                  <div class="popup-content">
                                     <span class="close" onclick="closePopup()">&times;</span>
-                                    <h2>Change Password</h2>
+                                    <h4>Change Password</h4>
 
                                     <form method="POST" action="">
-                                       <label for="currentPassword">Current Password:</label>
-                                       <input type="password" id="currentPassword" name="currentPassword"  required><br><br>
+                                       
+                                       <input type="password" id="currentPassword" name="currentPassword" placeholder="Current Password"  required><br><br>
 
-                                       <label for="newPassword">New Password:</label>
-                                       <input type="password" id="newPassword" name="newPassword"  required><br><br>
+                                      
+                                       <input type="password" id="newPassword" name="newPassword" placeholder="New Password" required><br><br>
 
-                                       <label for="confirmPassword">Confirm Password:</label>
-                                       <input type="password" id="confirmPassword" name="confirmPassword"  required><br><br>
+                                      
+                                       <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password"  required><br><br>
 
                                        <input type="submit" value="Submit" name="submit">
                                     </form>
@@ -174,7 +148,7 @@ input[type="submit"]:hover {
                               </div>
                               <a onclick="openPopup()"><button class="outline-btn outline-btn-blue" >
                                  Change Password</button></a><br><br>
-                                 <a href="./logout.php"><button class="outline-btn outline-btn-blue" >
+                              <a href="logout.php"><button class="outline-btn outline-btn-blue" >
                                  Log Out</button></a>
 
 
@@ -184,10 +158,10 @@ input[type="submit"]:hover {
 
 
                         <div class="col-lg-4">
-                        <a href="view-your-bookings.php"><div class="price-table-summary">
+                           <div class="price-table-summary">
                               <h4 class="bg-title">
-                                 View Your Bookings</h4>
-                           </div></a>
+                                 <a href="view-your-bookings.php">View Your Bookings</a></h4>
+                           </div>
                            
                            <div class="widget-bg widget-support-wrap">
                               <div class="icon">
